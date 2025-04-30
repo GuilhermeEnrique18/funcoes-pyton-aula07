82015a671e5b386a88cc05fec8e5adc82277e8e0
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-30 11:03:57 -0300

    adicao da funcao estoque para calcular o estoque com base na quantidade x valorUnitario

--- biblioteca.py
+++ biblioteca.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+
 def imprimeNome(nome):
     print(f"Nome: {nome}")
 
@@ -5,4 +8,13 @@ def solicitarNome():
     nome = input("Digite seu nome: ")
     return nome
 
-def
+def contaVogais(texto):
+    cont = 0
+    for i in range(0, len(texto)):
+        if texto[i] == "a" or texto[i] == "e" or texto[i] == "i" or texto[i] == "o" or texto[i] == "u":
+            cont += 1
+    print(cont)
+
+def estoque (produto,qtd,valorUnitario):
+    valorTotal = qtd * valorUnitario
+    return valorTotal
--- exercicio01.py
+++ exercicio01.py
@@ -1,8 +1,9 @@
-teste = []
-numeroDigitado = int(input("Informe um numero para fazer uma árvore: "))
-i=0
-for i in range (numeroDigitado):
-    i += 1
-    teste.append(i)
-    print(teste)
+from biblioteca import estoque
+nomeProduto = input("Digite o produto: ")
+quantidade = int(input("Digite a quantidade que você tem do produto em estoque: "))
+valorUnit = float(input("Digite o valor unitário do produto: "))
+
+retorno = estoque(nomeProduto, quantidade, valorUnit)
+
+print(retorno)
 
